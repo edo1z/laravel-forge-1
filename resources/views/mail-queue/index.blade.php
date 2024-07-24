dde5d006dddc68edff4c485c4b51bb8faaf70e43
edo1z <endo.yuta@logicky.com>
2024-07-24 11:25:35 +0900

    view調整

--- resources/views/mail-queue/index.blade.php
+++ resources/views/mail-queue/index.blade.php
@@ -1,14 +1,15 @@
 <x-guest-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Mail Queue') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="flex justify-between items-center mb-4">
+                        <a href="{{ url('/') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">トップに戻る</a>
+                        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                            {{ __('メールキュー') }}
+                        </h2>
+                    </div>
+
                     @if (session('status'))
                         <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
                             {{ session('status') }}
