d8cb085c55044cb7c0c1538cf9d614996336232d
edo1z <endo.yuta@logicky.com>
2024-07-24 21:13:17 +0900

    install_horizon修正

--- install_horizon.php
+++ install_horizon.php
@@ -1,5 +1,9 @@
 <?php
 echo "install_horizon.php started";
+$env = $_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? getenv('APP_ENV');
+$installHorizon = $_ENV['INSTALL_HORIZON'] ?? $_SERVER['INSTALL_HORIZON'] ?? getenv('INSTALL_HORIZON');
+echo "APP_ENV: $env";
+echo "INSTALL_HORIZON: $installHorizon";
 if ($env === 'production' || $installHorizon === 'true') {
     $output = shell_exec('composer require laravel/horizon 2>&1');
     echo "Composer Output: $output";
